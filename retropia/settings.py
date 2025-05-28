38c77c56f92d45e9aa89be3de66196758a87a7e8
Emre S. <mbeebm15@gmail.com>
2025-05-29 02:36:30 +0300

    Fix logout button with POST form

--- retropia/settings.py
+++ retropia/settings.py
@@ -162,3 +162,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGOUT_REDIRECT_URL = '/'
