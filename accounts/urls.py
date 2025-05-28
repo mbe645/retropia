f84cc10d8e66b1021926959d4b725562a31b3e6e
Emre S. <mbeebm15@gmail.com>
2025-05-29 01:35:36 +0300

    Add temporary route to create superuser on Render

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/<str:username>/', views.profile_view, name='profile_view'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
+    path('createsuperuser/', create_superuser),
 ]
