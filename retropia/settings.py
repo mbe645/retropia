a811009903988617f0ddf381624dadfe0b0185b8
Emre S. <mbeebm15@gmail.com>
2025-05-28 21:43:58 +0300

    Add STATIC and MEDIA configuration for deployment

--- retropia/settings.py
+++ retropia/settings.py
@@ -144,8 +144,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (user uploaded content)
 MEDIA_URL = '/media/'
