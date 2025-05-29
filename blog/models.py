5719ad69d73b51c987df7c7c18c973c58f0d4db6
Emre S. <mbeebm15@gmail.com>
2025-05-29 14:41:55 +0300

    Fix blog comments related_name

--- blog/models.py
+++ blog/models.py
@@ -12,10 +12,14 @@ class BlogPost(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blogpost-detail', args=[str(self.id)])
+        return reverse('blog:blog-detail', args=[str(self.id)])
 
 class BlogComment(models.Model):
-    blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='blog_comments')
+    blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')  # << önemli: 'comments' ile erişeceğiz
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.blog.title}"
+
--- blog/views.py
+++ blog/views.py
@@ -1,63 +1,23 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from .models import BlogPost
 from .forms import BlogPostForm, BlogCommentForm
-from django.http import HttpResponseForbidden
-from comments.models import Comment
-from comments.forms import CommentForm
 
 def index(request):
-    return HttpResponse("Welcome to the Blog section.")
+    return render(request, 'blog/index.html')
 
-@login_required
-def create_blogpost(request):
-    if request.method == 'POST':
-        form = BlogPostForm(request.POST)
-        if form.is_valid():
-            blogpost = form.save(commit=False)
-            blogpost.author = request.user
-            blogpost.save()
-            return redirect('blog-home')
-    else:
-        form = BlogPostForm()
-    return render(request, 'blog/blogpost_form.html', {'form': form})
-
-@login_required
-def edit_blogpost(request, pk):
-    blogpost = get_object_or_404(BlogPost, pk=pk)
-    if blogpost.author != request.user:
-        return HttpResponseForbidden('You are not allowed to edit this post.')
-    if request.method == 'POST':
-        form = BlogPostForm(request.POST, instance=blogpost)
-        if form.is_valid():
-            form.save()
-            return redirect('blog-home')
-    else:
-        form = BlogPostForm(instance=blogpost)
-    return render(request, 'blog/blogpost_form.html', {'form': form, 'edit_mode': True})
-
-@login_required
-def blogpost_detail(request, pk):
-    blogpost = get_object_or_404(BlogPost, pk=pk)
-    comments = blogpost.comments.all().order_by('-created_at')
-    comment_form = CommentForm()
-    return render(request, 'blog/blogpost_detail.html', {
-        'blogpost': blogpost,
-        'comments': comments,
-        'comment_form': comment_form
-    })
-
-@login_required
 def blog_list(request):
     posts = BlogPost.objects.all().order_by('-created_at')
     return render(request, 'blog/blog_list.html', {'posts': posts})
 
-@login_required
 def blog_detail(request, pk):
     post = get_object_or_404(BlogPost, pk=pk)
-    comments = post.comments.all()
+    comments = post.blog_comments.all().order_by('-created_at')
+
     if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return redirect('login')
         form = BlogCommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
@@ -71,4 +31,36 @@ def blog_detail(request, pk):
         'post': post,
         'form': form,
         'comments': comments,
+    })
+
+@login_required
+def create_blogpost(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            blogpost = form.save(commit=False)
+            blogpost.author = request.user
+            blogpost.save()
+            return redirect('blog:blog-detail', pk=blogpost.pk)
+    else:
+        form = BlogPostForm()
+    return render(request, 'blog/blogpost_form.html', {'form': form})
+
+@login_required
+def edit_blogpost(request, pk):
+    blogpost = get_object_or_404(BlogPost, pk=pk)
+    if blogpost.author != request.user:
+        return HttpResponseForbidden('You are not allowed to edit this post.')
+    
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, instance=blogpost)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:blog-detail', pk=pk)
+    else:
+        form = BlogPostForm(instance=blogpost)
+    
+    return render(request, 'blog/blogpost_form.html', {
+        'form': form,
+        'edit_mode': True
     })
