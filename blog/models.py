3aee769c36c9a10e2339e9d183451beed17a30e3
Emre S. <mbeebm15@gmail.com>
2025-05-29 16:11:26 +0300

    Fix 500 error by making BlogComment __str__ method safe

--- blog/models.py
+++ blog/models.py
@@ -15,11 +15,12 @@ class BlogPost(models.Model):
         return reverse('blog:blog-detail', args=[str(self.id)])
 
 class BlogComment(models.Model):
-    blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')  # << önemli: 'comments' ile erişeceğiz
+    blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.user.username} on {self.blog.title}"
-
+        user_str = self.user.username if self.user else "Unknown User"
+        blog_str = self.blog.title if self.blog else "Unknown Blog"
+        return f"Comment by {user_str} on {blog_str}"
