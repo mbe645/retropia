6bcc18e58f16b978d658f01a187f367482ff24c3
Emre S. <mbeebm15@gmail.com>
2025-05-29 13:38:12 +0300

    Fix: Ensure all Page slugs are set and auto-generated in admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Page
 
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "slug")
+
+admin.site.register(Page, PageAdmin)
